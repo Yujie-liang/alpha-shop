a4e1852548257b76367a00f4934ea32cbbdd1552
Yujie-liang <yujieliang0404@gmail.com>
2024-06-24 12:21:31 +0800

    add toggle favorite function

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,13 @@
 import styles from './Products.module.scss';
 import { useState, useEffect } from 'react';
 import { useProducts } from '../../context/ProductsContext';
-
+import useFavoriteItems from '../../context/FavoriteContext';
+import { useAuth } from '../../context/AuthContext';
 export default function Products({ name, price, imgUrl }) {
   const { filteredProducts } = useProducts();
+  const { currentMember } = useAuth();
+  const [favorites, toggleFavorite] = useFavoriteItems(currentMember);
+
   if (filteredProducts.length === 0) return (<h2 className={styles.note}>沒有符合條件的商品!</h2>)
   return (
     <div className={`container ${styles.products}`}>
@@ -17,7 +21,7 @@ export default function Products({ name, price, imgUrl }) {
                 e.currentTarget.style.backgroundImage = '';
               }}></div>
             <div className={styles.addToFavorite}>
-              <i className={`${styles.tshirtIcon} fa-regular fa-heart`}></i>
+              <i className={`${styles.tshirtIcon} ${favorites.includes(product.id) ? 'fa-solid' : 'fa-regular'} fa-heart`} onClick={()=>toggleFavorite(product.id)}></i>
             </div>
             <div className={styles.tshirtText}>
               <p className={styles.tshirtName}>{product.name}</p>
