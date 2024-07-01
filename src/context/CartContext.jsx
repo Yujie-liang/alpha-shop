ab6c64a36ea2987f19b971929b5abbe1754aa413
Yujie-liang <yujieliang0404@gmail.com>
2024-07-01 09:30:25 +0800

    change baseUrl

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,8 +4,8 @@ import { useAuth } from './AuthContext';
 import { useProducts } from './ProductsContext';
 import axios from 'axios';
 
-// const baseUrl = 'http://localhost:3004';
-const baseUrl = 'https://us-central1-alpha-shop-api.cloudfunctions.net/api';
+const baseUrl = 'http://localhost:3004';
+// const baseUrl = 'https://us-central1-alpha-shop-api.cloudfunctions.net/api';
   
 export const CartContext = createContext();
 export function CartProvider({children}){
